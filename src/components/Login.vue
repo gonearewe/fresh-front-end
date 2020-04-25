ddf2a6e7767c5243051c3b239daf1a5ef0349394
gonearewe <cristmactavish@outlook.com>
2020-04-25 21:54:03 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    UserName:<input type="text" v-model="loginForm.username" placeholder="Please enter your username"/>
+    <br/>
+    Password:<input type="text" v-model="loginForm.password" placeholder="Please enter your password"/>
+    <br/>
+    <button v-on:click="login">Login</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Login',
+    data(){
+      return {
+        loginForm:{
+          username:'',
+          password:''
+        },
+        responseResult:[]
+      }
+    },
+  }
+</script>
