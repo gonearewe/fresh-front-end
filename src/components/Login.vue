b69ff79df0c6450a52f4616a13b09c42d9b0a97c
gonearewe <cristmactavish@outlook.com>
2020-04-26 21:01:40 +0800

    finish login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,47 @@
 <template>
   <div>
-    UserName:<input type="text" v-model="loginForm.username" placeholder="Please enter your username"/>
+    UserName:
+    <label>
+      <input type="text" v-model="loginForm.username" placeholder="Please enter your username"/>
+    </label>
     <br/>
-    Password:<input type="text" v-model="loginForm.password" placeholder="Please enter your password"/>
+    Password:
+    <label>
+      <input type="text" v-model="loginForm.password" placeholder="Please enter your password"/>
+    </label>
     <br/>
     <button v-on:click="login">Login</button>
   </div>
 </template>
 
 <script>
-  export default {
-    name:'Login',
-    data(){
-      return {
-        loginForm:{
-          username:'',
-          password:''
-        },
-        responseResult:[]
-      }
-    },
+export default {
+  name: 'Login',
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      responseResult: []
+    }
+  },
+  methods: {
+    login () {
+      console.log('here')
+      this.$axios.post('/login', {
+        username: this.loginForm.username,
+        password: this.loginForm.password
+      }).then(successResult => {
+        console.log('the')
+        if (successResult.data.code === 200) {
+          console.log('success')
+          this.$router.replace({ path: '/index' })
+        }
+      }).catch(failureResult => {
+        console.log(failureResult)
+      })
+    }
   }
+}
 </script>
