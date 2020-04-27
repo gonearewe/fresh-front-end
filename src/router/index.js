81d603d3d0dd1fa256cd5fd7a20e7bf3db93ad22
gonearewe <cristmactavish@outlook.com>
2020-04-27 19:28:24 +0800

    almost finished

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// 导入刚才编写的组件
+import Home from '../components/Home'
 import AppIndex from '@/components/home/AppIndex'
 import Login from '@/components/Login'
+import LibraryIndex from '../components/library/LibraryIndex'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
-    // 下面都是固定的写法
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      // home页面并不需要被访问
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/library',
+          name: 'Library',
+          component: LibraryIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
+    },
     {
       path: '/login',
       name: 'Login',
       component: Login
-    },
-    {
-      path: '/index',
-      name: 'AppIndex',
-      component: AppIndex
     }
   ]
 })
